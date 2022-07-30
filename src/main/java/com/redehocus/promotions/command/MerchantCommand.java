40dd5f10b85cedeca7ec9cd458a2a8b36f4587f3
onlyafter <afterzinndelas@gmail.com>
2022-07-30 17:45:20 +0000

    added forceupdate command and sound on flush

--- src/main/java/com/redehocus/promotions/command/MerchantCommand.java
+++ src/main/java/com/redehocus/promotions/command/MerchantCommand.java
@@ -3,16 +3,31 @@ package com.redehocus.promotions.command;
 import com.google.common.collect.ImmutableMap;
 import com.redehocus.promotions.PromotionsPlugin;
 import com.redehocus.promotions.handler.PromotionHandler;
+import com.redehocus.promotions.thread.PromotionFlushThread;
 import com.redehocus.promotions.view.PromotionView;
+import me.lucko.helper.Schedulers;
 import me.saiintbrisson.minecraft.command.annotation.Command;
 import me.saiintbrisson.minecraft.command.command.Context;
 import org.bukkit.entity.Player;
 
+import java.util.concurrent.TimeUnit;
+
 public class MerchantCommand {
 
-    @Command(name = "mercador")
+    @Command(name = "mercador", aliases = "merchant")
     public void handle(Context<Player> context) {
         PromotionsPlugin.getInstance().getViewFrame().open(PromotionView.class, context.getSender(), ImmutableMap.of("promotion", PromotionHandler.PROMOTION_ITEM));
+
+    }
+
+    @Command(
+            name = "mercador.forceupdate",
+            aliases = { "merchant.forceupdate" },
+            permission = "merchant.admin"
+    )
+    public void handleUpdate(Context<Player> context) {
+        Schedulers.async().runLater(new PromotionFlushThread(), 1, TimeUnit.SECONDS);
+        context.sendMessage("§eAtualização forçada realizada com êxito.");
 
     }
 
--- src/main/java/com/redehocus/promotions/thread/PromotionFlushThread.java
+++ src/main/java/com/redehocus/promotions/thread/PromotionFlushThread.java
@@ -2,41 +2,43 @@ package com.redehocus.promotions.thread;
 
 import com.redehocus.promotions.handler.PromotionHandler;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.github.paperspigot.Title;
 
 import java.util.concurrent.TimeUnit;
 
-public class PromotionFlushThread implements Runnable {
-    public static long LAST_UPDATE = 0L;
+    public class PromotionFlushThread implements Runnable {
+        public static long LAST_UPDATE = 0L;
 
-    @Override
-    public void run() {
+        @Override
+        public void run() {
 
-        PromotionHandler.update();
+            PromotionHandler.update();
 
-        Bukkit.getOnlinePlayers().forEach(player -> {
+            Bukkit.getOnlinePlayers().forEach(player -> {
 
-            Title title = Title.builder()
-                    .fadeOut(20)
-                    .fadeIn(20)
-                    .stay(40)
-                    .title("§e§lMercador atualizado!")
-                    .subtitle("§f1 novo produto chegou na loja!")
-                    .build();
+                Title title = Title.builder()
+                        .fadeOut(20)
+                        .fadeIn(20)
+                        .stay(40)
+                        .title("§e§lMercador atualizado!")
+                        .subtitle("§f1 novo produto chegou na loja!")
+                        .build();
 
 
-            player.sendTitle(title);
-            player.sendMessage(new String[] {
-                    "",
-                    "§e§l Mercador atualizado!",
-                    "§f Um novo produto chegou na loja",
-                    "§f corra para conferir o item.",
-                    ""
+                player.sendTitle(title);
+                player.playSound(player.getLocation(), Sound.CHEST_OPEN, 1, 1);
+                player.sendMessage(new String[] {
+                        "",
+                        "§e§l Mercador atualizado!",
+                        "§f Um novo produto chegou na loja",
+                        "§f corra para conferir o item.",
+                        ""
+                });
+
             });
 
-        });
+            LAST_UPDATE = System.currentTimeMillis();
 
-        LAST_UPDATE = System.currentTimeMillis();
-
+        }
     }
-}
